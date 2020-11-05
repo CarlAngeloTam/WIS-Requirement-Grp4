f84c229162993aaae47d20b1fbf18b5cdabf9dce
VivieneQuidilla <viene.quidilla17@gmail.com>
2020-11-05 17:05:06 +0800

    Create 2020_11_05_082209_create_books_table.php
    migration books table

---
+++ database/migrations/2020_11_05_082209_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            //new book tables
+             $table->string('Title', 45);
+             $table->string('Author', 45);
+             $table->string('Category', 45);
+             $table->date('Rent_Date');
+             $table->date('Due_Date');
+             $table->decimal('Penalty', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
